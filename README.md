3cf46969a6a8736f5c9af3158e6988fac2ec378f
Aditya Agarwal <adityaa803@gmail.com>
2018-04-15 11:26:03 +0530

    basic working version ready

---
+++ README.md
@@ -0,0 +1,15 @@
+# react-preview
+
+> a GitHub App built with [probot](https://github.com/probot/probot) that generates preview links for react based projects.
+
+## Setup
+
+```sh
+# Install dependencies
+npm install
+
+# Run the bot
+npm start
+```
+
+See [docs/deploy.md](docs/deploy.md) if you would like to run your own instance of this app.
---
+++ index.js
@@ -0,0 +1,40 @@
+const commands = require('probot-commands')
+
+const getCodeSandboxLink = ({owner, repo, branch, folder = ''}) => {
+  const link = `https://codesandbox.io/s/github/${owner}/${repo}/tree/${branch}/${folder}`
+  return link
+}
+
+module.exports = (robot) => {
+  commands(robot, 'preview', async (context, command) => {
+    const { payload } = context
+    if (payload.comment.user.type === 'Bot') {
+      return
+    }
+
+    const config = await context.config('config.yml', {
+      reactPreviewFolder: ''
+    })
+    
+    const repo = payload.repository.name
+    const owner = payload.repository.owner.login
+    const prNum = payload.issue.number
+
+    const { data: pullRequestResult } = await context.github.pullRequests.get({owner, repo, number: prNum})
+
+    const prBranchName = pullRequestResult.head.ref
+    const prRepoName = pullRequestResult.head.repo.name
+    const prOwnerName = pullRequestResult.head.repo.owner.login
+
+    const link = getCodeSandboxLink({
+      owner: prOwnerName,
+      repo: prRepoName,
+      branch: prBranchName,
+      folder: config.reactPreviewFolder
+    })
+
+    context.github.issues.createComment(context.issue({
+      body: `preview it [here](${link})`
+    }))
+  })
+}
